56db5ca09403116a996b64806e15a759c5db1a31
esatuae-cell <esatuae@gmail.com>
2026-07-20 17:27:52 +0400

    navigation hide
    navigation hide

--- src/app/widget/common/navigation/navigation.ts
+++ src/app/widget/common/navigation/navigation.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { Route } from '@angular/router';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -9,20 +8,41 @@ import { RouterLink } from '@angular/router';
   styleUrl: './navigation.css',
 })
 export class Navigation {
+  activeMenu: string | null = null;
 
-activeMenu: string | null = null;
+  isDropdownOpen = false;
+  clicked = false;
 
 
-toggleMenu(menu: string) {
-
-  if (this.activeMenu === menu) {
-    this.activeMenu = null;
-  } 
-  else {
-    this.activeMenu = menu;
+  closeDropdown() {
+    this.isDropdownOpen = false;
   }
 
-}
+  showDropdown() {
+    this.isDropdownOpen = true;
+  }
 
+  hideDropdown() {
+    if (!this.clicked) {
+      this.isDropdownOpen = false;
+    }
+  }
 
+  toggleClickDropdown(event: Event) {
+    this.clicked = true;
+    this.isDropdownOpen = false;
+
+    // allow hover again after mouse leaves
+    setTimeout(() => {
+      this.clicked = false;
+    }, 300);
+  }
+
+  toggleMenu(menu: string) {
+    if (this.activeMenu === menu) {
+      this.activeMenu = null;
+    } else {
+      this.activeMenu = menu;
+    }
+  }
 }
