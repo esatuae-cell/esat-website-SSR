184a37a4743c53df3ec7414ba73f936e6ad09e65
esatuae-cell <esatuae@gmail.com>
2026-07-29 13:55:28 +0400

    story-page-update
    story-page-update

--- src/app/widget/pages/about/our-stories/our-stories.ts
+++ src/app/widget/pages/about/our-stories/our-stories.ts
@@ -1,7 +1,7 @@
-import { Component, computed, signal,HostListener  } from '@angular/core';
+import { Component, computed, signal, HostListener } from '@angular/core';
 import { STORIES } from './our-stories.data';
 import { CommonModule } from '@angular/common';
-import { About } from "../about/about";
+import { About } from '../about/about';
 
 interface Story {
   year: number;
@@ -10,16 +10,14 @@ interface Story {
   image?: string;
 }
 
-
 @Component({
   selector: 'app-our-stories',
   standalone: true,
-  imports: [CommonModule, ],
+  imports: [CommonModule],
   templateUrl: './our-stories.html',
   styleUrl: './our-stories.css',
 })
 export class OurStories {
-
   stories: Story[] = STORIES;
 
   // active index
@@ -30,12 +28,10 @@ export class OurStories {
   windowSize = 10;
 
   visibleStories = computed(() =>
-    this.stories.slice(this.startIndex(), this.startIndex() + this.windowSize)
+    this.stories.slice(this.startIndex(), this.startIndex() + this.windowSize),
   );
 
-  activeStory = computed(() =>
-    this.stories[this.activeIndex()]
-  );
+  activeStory = computed(() => this.stories[this.activeIndex()]);
 
   selectYear(index: number) {
     const globalIndex = this.startIndex() + index;
@@ -43,14 +39,26 @@ export class OurStories {
   }
 
   next() {
-    if (this.startIndex() + this.windowSize < this.stories.length) {
-      this.startIndex.update(v => v + 1);
+    // Move active year forward
+    if (this.activeIndex() < this.stories.length - 1) {
+      this.activeIndex.update((v) => v + 1);
+    }
+
+    // Move visible window forward if active year reaches the end
+    if (this.activeIndex() >= this.startIndex() + this.windowSize) {
+      this.startIndex.update((v) => v + 1);
     }
   }
 
   prev() {
-    if (this.startIndex() > 0) {
-      this.startIndex.update(v => v - 1);
+    // Move active year backward
+    if (this.activeIndex() > 0) {
+      this.activeIndex.update((v) => v - 1);
+    }
+
+    // Move visible window backward if active year reaches the beginning
+    if (this.activeIndex() < this.startIndex()) {
+      this.startIndex.update((v) => v - 1);
     }
   }
 
