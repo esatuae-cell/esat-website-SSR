a430011d4b81b12b1f635dc40579d4bf2613c8d1
esatuae-cell <esatuae@gmail.com>
2026-07-31 18:06:20 +0400

    contact page
    contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+
 import { HomeComponent } from './widget/pages/home/home';
 import { Partnership } from './widget/pages/partnership/partnership';
 import { Careers } from './widget/pages/careers/careers';
@@ -7,46 +8,72 @@ import { TrailComponent } from './widget/pages/trail/trail';
 import { Advertisement } from './widget/pages/advertisement/advertisement';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  {
+    path: '',
+    component: HomeComponent,
+  },
 
-    { path: 'home', component: HomeComponent },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+
   {
     path: 'about',
-    loadChildren: () => 
-      import('../app/widget/pages/about/about-module').then((m) => m.AboutModule),
+    loadChildren: () => import('./widget/pages/about/about-module').then((m) => m.AboutModule),
   },
+
   {
     path: 'software',
     loadChildren: () =>
-      import('../app/widget/pages/software/software-module').then((m) => m.SoftwareModule),
+      import('./widget/pages/software/software-module').then((m) => m.SoftwareModule),
   },
-   {
+
+  {
     path: 'hardware',
     loadChildren: () =>
-      import('../app/widget/pages/hardware/hardware-module').then((m) => m.HardwareModule),
+      import('./widget/pages/hardware/hardware-module').then((m) => m.HardwareModule),
   },
-  
-   { path: 'partnership', component: Partnership },
 
-   { path: 'careers', component: Careers },
+  {
+    path: 'partnership',
+    component: Partnership,
+  },
 
-  { path: 'our-clients', component: Clients },
+  {
+    path: 'careers',
+    component: Careers,
+  },
+
+  {
+    path: 'our-clients',
+    component: Clients,
+  },
 
   {
     path: 'support',
     loadChildren: () =>
-      import('../app/widget/pages/support/support-module').then((m) => m.SupportModule),
+      import('./widget/pages/support/support-module').then((m) => m.SupportModule),
   },
 
   {
     path: 'contact',
     loadChildren: () =>
-      import('../app/widget/pages/contact/contact-module').then((m) => m.ContactModule),
+      import('./widget/pages/contact/contact-module').then((m) => m.ContactModule),
   },
 
-    { path: 'free-trail', component: TrailComponent },
+  {
+    path: 'free-trail',
+    component: TrailComponent,
+  },
 
+  {
+    path: 'advertisement',
+    component: Advertisement,
+  },
 
-     { path: 'advertisement', component:Advertisement },
-  
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
