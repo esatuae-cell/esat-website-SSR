26201214a12f72a4762fea8aee51a84a22cbfe6b
esatuae-cell <esatuae@gmail.com>
2026-07-31 17:17:04 +0400

    changes
    changes

--- src/app/widget/pages/software/software-support/software-support.ts
+++ src/app/widget/pages/software/software-support/software-support.ts
@@ -1,38 +1,108 @@
 import { Component, OnInit } from '@angular/core';
-import { RootServices } from '../../../../services/root-services';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { SwCommon } from "../sw-common/sw-common";
+
+import { RootServices } from '../../../../services/root-services';
+import { SwCommon } from '../sw-common/sw-common';
 
 @Component({
   selector: 'app-software-support',
-  imports: [SwCommon,RouterLink],
+  standalone: true,
+  imports: [SwCommon, RouterLink],
   templateUrl: './software-support.html',
   styleUrl: './software-support.css',
 })
 export class SoftwareSupport implements OnInit {
-ChangeHover($event: Event) {
-throw new Error('Method not implemented.');
-}
-  public textValue: string = '';
-  public queone: boolean = false;
-  public quetwo: boolean = false;
-  public quethree: boolean = false;
-  id: any;
-  private sub: any;
-  constructor(public route: ActivatedRoute, public $rootScope: RootServices, ) {
-  
+  // --------------------------------------------------
+  // State
+  // --------------------------------------------------
+
+  public textValue = '';
+
+  public queone = false;
+  public quetwo = false;
+  public quethree = false;
+
+  public id: string = '';
+
+  // --------------------------------------------------
+  // Constructor
+  // --------------------------------------------------
+
+  constructor(
+    public route: ActivatedRoute,
+    public $rootScope: RootServices,
+  ) {}
+
+  // --------------------------------------------------
+  // Init
+  // --------------------------------------------------
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.id = params['id'] || 'ser1';
+
+      this.changeCircle(this.id);
+    });
   }
 
-  ngOnInit() {
-    this.sub = this.route.params.subscribe(params => {
-      this.id = params['id'];
-    });
-    setTimeout(() => {
-      this.changeCircle(this.id);
-    }), 1000;
+  // --------------------------------------------------
+  // Change active service
+  // --------------------------------------------------
+
+  changeCircle(id: string): void {
+    switch (id) {
+      case 'ser1':
+        this.textValue = 'ser1';
+
+        this.queone = false;
+        this.quetwo = true;
+        this.quethree = true;
+
+        break;
+
+      case 'ser2':
+        this.textValue = 'ser2';
+
+        this.queone = true;
+        this.quetwo = false;
+        this.quethree = true;
+
+        break;
+
+      case 'ser3':
+        this.textValue = 'ser3';
+
+        this.queone = true;
+        this.quetwo = true;
+        this.quethree = false;
+
+        break;
+
+      default:
+        this.textValue = 'ser1';
+
+        this.queone = false;
+        this.quetwo = true;
+        this.quethree = true;
+
+        break;
+    }
   }
-  changeCircle(id: any) {
-    throw new Error('Method not implemented.');
+
+  // --------------------------------------------------
+  // Click outside
+  // --------------------------------------------------
+
+  ChangeHover(_event: Event): void {
+    // Intentionally empty.
+    //
+    // This method exists because the template calls:
+    //
+    // (clickOutside)="ChangeHover($event)"
+    //
+    // Do not throw an error here.
+    //
+    // If you later need DOM manipulation, make it SSR-safe
+    // with isPlatformBrowser().
   }
- 
 }
